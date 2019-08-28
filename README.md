58537f8156c34582d9f40f73536be3998e10e163
sud0301 <mittal@quimby.informatik.privat>
2019-08-28 17:23:33 +0200

    update

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
-## Accuracy
-| Model             |  Accuracy   |
-| ----------------- | ----------- |
-| WRN-28-10, 4k, FSL|   85.7      |
-| WRN-28-10, 4k, SSL|   88.7      |
+## Pytorch Implementation of Unsupervised Data Augmentation
+
+Model: WRN-28-2
+
+| Model                 | FSL        |   SSL      | SSL (Paper) |
+| -----------------     | ---        | -------    | ----        |
+| 250                   | 55.5       | 13.0       | 8.41        |
+| 500                   | 42.8       | 11.5       | 6.91        |
+| 1000                  | 29.7       | 8.5        | 6.39        |
+| 2000                  | 20.1       | 6.9        | 5.84        |
+| 4000                  | 14.3       | 6.2        | 5.27        |
+| All                   | 4.2        | ---        | ----        |
+
+
+
+
 
--- main.py
+++ main.py
@@ -20,35 +20,53 @@ import numpy as np
 from utils import progress_bar
 from cutout import Cutout
 
-#from data_loader import iCIFAR10
 #from resnet import resnet18
 from wrn import wrn
+#from autoaugment_extra_only_color import CIFAR10Policy
 from autoaugment_extra import CIFAR10Policy
 
-parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+SEED = 0
+SPLIT_ID = None
+
+parser = argparse.ArgumentParser(description='PyTorch SSL CIFAR10 UDA Training')
 parser.add_argument('--lr', default=0.03, type=float, help='learning rate')
-parser.add_argument('--lr-warm-up', action='store_true', help='increase lr slowly')
-parser.add_argument('--warm-up-steps', default=20000, type=int, help='number of iterations for warmup')
+parser.add_argument('--softmax-temp', default=-1, type=float, help='softmax temperature controlling')
+parser.add_argument('--confidence-mask', default=-1, type=float, help='Confidence value for masking')
+
+parser.add_argument('--num-labeled', default=1000, type=int, help='number of labeled_samples')
 
 parser.add_argument('--batch-size-lab', default=64, type=int, help='training batch size')
 parser.add_argument('--batch-size-unlab', default=320, type=int, help='training batch size')
 parser.add_argument('--num-steps', default=100000, type=int, help='number of iterations')
+parser.add_argument('--lr-warm-up', action='store_true', help='increase lr slowly')
+parser.add_argument('--warm-up-steps', default=20000, type=int, help='number of iterations for warmup')
+parser.add_argument('--num-cycles', default=4, type=int, help='number of sgdr cycles')
 
-parser.add_argument('--partial-data', default=0.5, type=float, help='partial data')
-parser.add_argument("--partial-id", type=str, default=None, help="restore partial id list")
+parser.add_argument("--split-id", type=str, default=SPLIT_ID, help="restore partial id list")
 parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
+parser.add_argument('--verbose', action='store_true', help='show progress bar')
+parser.add_argument('--seed', default=SEED, type=int, help='seed index')
 
+# Supervised or Semi-supervised
+parser.add_argument('--lab-only', action='store_true', help='if using only labeled samples')
+
+# Augmenatations
 parser.add_argument('--cutout', action='store_true', help='use cutout augmentation')
 parser.add_argument('--n-holes', default=1, type=float, help='number of holes for cutout')
 parser.add_argument('--cutout-size', default=16, type=float, help='size of the cutout window')
-
 parser.add_argument('--autoaugment', action='store_true', help='use autoaugment augmentation')
-parser.add_argument('--GCN', action='store_true', help='resume from checkpoint')
+
 args = parser.parse_args()
+
+CHECKPOINT_DIR = './results/labels_' + str(args.num_labeled) + '_batch_lab_' +  str(args.batch_size_lab) + '_batch_unlab_' + str(args.batch_size_unlab)  + '_steps_' + str(args.num_steps) +'_warmup_' + str(args.warm_up_steps) + '_softmax_temp_' + str(args.softmax_temp) + '_conf_mask_' + str(args.confidence_mask) + '_SEED_' + str(args.seed)
+
+if not os.path.exists(CHECKPOINT_DIR):
+    os.makedirs(CHECKPOINT_DIR)
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 best_acc = 0  # best test accuracy
 start_epoch = 1  # start from epoch 0 or last checkpoint epoch
+np.random.seed(args.seed)
 
 class TransformTwice:
     def __init__(self, transform, aug_transform):
@@ -63,9 +81,6 @@ class TransformTwice:
 # Data
 print('==> Preparing data..')
 transform_ori = transforms.Compose([
-    transforms.ToTensor(),
-    #transforms.Normalize((0.49139968, 0.48215841, 0.44653091), (0.24703223, 0.24348513, 0.26158784)),
-    transforms.ToPILImage(),
     transforms.RandomCrop(32, padding=4, padding_mode='reflect'),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
@@ -75,7 +90,6 @@ transform_ori = transforms.Compose([
 transform_aug = transforms.Compose([
     CIFAR10Policy(),
     transforms.ToTensor(),
-    #transforms.Normalize((0.49139968, 0.48215841, 0.44653091), (0.24703223, 0.24348513, 0.26158784)),
     Cutout(n_holes=args.n_holes, length=args.cutout_size),
     transforms.ToPILImage(),
     transforms.RandomCrop(32, padding=4, padding_mode='reflect'),
@@ -87,13 +101,10 @@ transform_aug = transforms.Compose([
 
 transform_test = transforms.Compose([
     transforms.ToTensor(),
-    #transforms.Normalize((0.49139968, 0.48215841, 0.44653091), (0.24703223, 0.24348513, 0.26158784)),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
 ])
 
 transform_train = TransformTwice(transform_ori, transform_aug)
-
-#list_classes = [5,6,7,8,9]
 
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
 labelset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_test)
@@ -101,25 +112,28 @@ labelset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True
 #trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=8)
 
 train_dataset_size = len(trainset)
-partial_size = int(args.partial_data * train_dataset_size)
 
+#if args.partial_id is not None:
+#train_ids = pickle.load(open(args.split_id, 'rb'))
+#print('loading train ids from {}'.format(args.split_id))
+#else:
+#    train_ids = np.arange(train_dataset_size)
+#    np.random.shuffle(train_ids)
 
-if args.partial_id is not None:
-    train_ids = pickle.load(open(args.partial_id, 'rb'))
-    print('loading train ids from {}'.format(args.partial_id))
+#pickle.dump(train_ids, open('train_id.pkl', 'wb'))
+
+if args.split_id is not None:
+    train_ids = pickle.load(open(args.split_id, 'rb'))
+    print('loading train ids from {}'.format(args.split_id))
 else:
     train_ids = np.arange(train_dataset_size)
     np.random.shuffle(train_ids)
-
-pickle.dump(train_ids, open('train_id.pkl', 'wb'))
-
-#train_sampler_lab = data.sampler.SubsetRandomSampler(train_ids[:4000])
-#train_sampler_unlab = data.sampler.SubsetRandomSampler(train_ids[4000:])
+    pickle.dump(train_ids, open(os.path.join(CHECKPOINT_DIR, 'train_id_' + str(args.seed) + '.pkl'), 'wb'))
 
 mask = np.zeros(train_ids.shape[0], dtype=np.bool)
 labels = np.array([trainset[i][1] for i in train_ids], dtype=np.int64)
 for i in range(10):
-    mask[np.where(labels == i)[0][: int(4000 / 10)]] = True
+    mask[np.where(labels == i)[0][: int(args.num_labeled / 10)]] = True
 labeled_indices, unlabeled_indices = train_ids[mask], train_ids[~ mask]
 #labeled_indices, unlabeled_indices = train_ids[mask], train_ids
 
@@ -134,24 +148,12 @@ trainloader_val = data.DataLoader(labelset, batch_size=100, sampler=train_sample
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=16)
 
-#trainloader_lab_iter = iter(trainloader_lab)
-#trainloader_unlab_iter = iter(trainloader_unlab)
-
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 # Model
 print('==> Building model..')
 net = wrn().cuda()
 
-'''
-def weights_init(m):
-    if isinstance(m, nn.Conv2d):
-        #torch.nn.init.xavier_uniform_(m.weight.data)
-        #weights_shape = []
-        print ('m size: ', m.size())
-        n = m.size(0)*m.size(1)*m.size(3)
-        torch.nn.init.normal_(m.weight.data, mean=0.0, std=np.sqrt(2.0/n))
-'''
 if device == 'cuda':
     net = torch.nn.DataParallel(net)
     cudnn.benchmark = True
@@ -170,16 +172,13 @@ optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5
 #optimizer = optim.Adam(net.parameters(), lr=args.lr, betas= (0.9, 0.999))
 scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.num_steps, eta_min=0.0001)
 
+def _kl_divergence_with_logits(p_logits, q_logits):
+    p = torch.nn.functional.softmax(p_logits, dim=1)
+    log_p = torch.nn.functional.log_softmax(p_logits, dim=1)
+    log_q = torch.nn.functional.log_softmax(q_logits, dim=1)
 
-def global_contrast_normalize(X, scale=55., min_divisor=1e-8):
-    X = X.view(X.size(0), -1)
-    X = X - X.mean(dim=1, keepdim=True)
-
-    normalizers = torch.sqrt( torch.pow( X, 2).sum(dim=1, keepdim=True)) / scale
-    normalizers[normalizers < min_divisor] = 1.
-    X /= normalizers
-
-    return X.view(X.size(0),3,32,32)
+    kl = torch.sum(p * (log_p - log_q), dim=1)
+    return kl
 
 def set_optimizer_lr(optimizer, lr):
     for param_group in optimizer.param_groups:
@@ -187,8 +186,8 @@ def set_optimizer_lr(optimizer, lr):
     return optimizer
 
 # Training
-def train(epoch, trainloader_lab, trainloader_unlab, scheduler, optimizer):
-    print('\nEpoch: %d' % epoch)
+def train(cycle, trainloader_lab, trainloader_unlab, scheduler, optimizer):
+    print('\nCycle: %d' % cycle)
     train_loss = 0
     train_loss_lab = 0
     train_loss_unlab = 0
@@ -198,8 +197,6 @@ def train(epoch, trainloader_lab, trainloader_unlab, scheduler, optimizer):
     trainloader_lab_iter = iter(trainloader_lab)
     trainloader_unlab_iter = iter(trainloader_unlab)
   
-    #net.apply(weights_init) 
-
     for i_iter in range(args.num_steps):
         net.train()
         optimizer.zero_grad()
@@ -222,43 +219,53 @@ def train(epoch, trainloader_lab, trainloader_unlab, scheduler, optimizer):
         (inputs_lab, _), targets_lab = batch_lab
         inputs_lab, targets_lab = inputs_lab.to(device), targets_lab.to(device)
        
-        if args.GCN: 
-            inputs_lab = global_contrast_normalize( inputs_lab )
         
         outputs_lab = net(inputs_lab)
         loss_lab = criterion(outputs_lab, targets_lab)
         #inputs, targets = inputs.to(device), targets.to(device)
-        
-        try:
-            batch_unlab = next(trainloader_unlab_iter)
-        except:
-            trainloader_unlab_iter = iter(trainloader_unlab)
-            batch_unlab = next(trainloader_unlab_iter) 
-        
-        (inputs_unlab, inputs_unlab_aug), _ = batch_unlab
-        inputs_unlab, inputs_unlab_aug = inputs_unlab.cuda(), inputs_unlab_aug.cuda()
-       
-        if args.GCN: 
-            inputs_unlab = global_contrast_normalize( inputs_unlab )
-            inputs_unlab_aug = global_contrast_normalize( inputs_unlab_aug )
 
-        outputs_unlab = net(inputs_unlab)
-        outputs_unlab_aug = net(inputs_unlab_aug)
-        #print (targets)
-        #loss_unlab = nn.KLDivLoss()(F.log_softmax(outputs_unlab), F.softmax(outputs_unlab_aug))
-        #print ('outputs unlab size(): ', outputs_unlab.size())
-        #loss_unlab = nn.KLDivLoss()(F.log_softmax(outputs_unlab_aug, dim=1), F.softmax(outputs_unlab, dim=1).detach(), reduction='batchmean')
-      
-        loss_unlab = torch.nn.functional.kl_div( 
-                     torch.nn.functional.log_softmax(outputs_unlab_aug, dim=1), 
-                     torch.nn.functional.softmax(outputs_unlab, dim=1).detach(), reduction='batchmean') 
-    
-        #loss_kldiv = F.kl_div(F.log_softmax(outputs_unlab_aug, dim=1), F.softmax(outputs_unlab, dim=1), reduction='none')    # loss for unsupervised
-        #loss_kldiv = torch.sum(loss_kldiv, dim=1)
-    
-        #loss_unlab =  1.0*torch.mean(loss_kldiv)
- 
-        loss = loss_lab + loss_unlab
+        if args.lab_only:  
+            loss_unlab = 0.0      
+            loss = loss_lab 
+        else:
+            try:
+                batch_unlab = next(trainloader_unlab_iter)
+            except:
+                trainloader_unlab_iter = iter(trainloader_unlab)
+                batch_unlab = next(trainloader_unlab_iter) 
+            
+            (inputs_unlab, inputs_unlab_aug), _ = batch_unlab
+            inputs_unlab, inputs_unlab_aug = inputs_unlab.cuda(), inputs_unlab_aug.cuda()
+           
+            outputs_unlab = net(inputs_unlab)
+            outputs_unlab_aug = net(inputs_unlab_aug)
+
+            if args.softmax_temp != -1:
+               
+                loss_unlab = _kl_divergence_with_logits(
+                        p_logits=(outputs_unlab/args.softmax_temp).detach(),
+                        q_logits=outputs_unlab_aug)
+         
+                if args.confidence_mask != -1:
+                    unlab_prob = torch.nn.functional.softmax(outputs_unlab, dim=1)
+                    largest_prob, _ = unlab_prob.max(1)
+                    mask = (largest_prob>args.confidence_mask).float().detach()
+                    loss_unlab = loss_unlab*mask 
+                
+                loss_unlab = torch.mean(loss_unlab)
+
+            else:
+                loss_unlab = torch.nn.functional.kl_div( 
+                         torch.nn.functional.log_softmax(outputs_unlab_aug, dim=1), 
+                         torch.nn.functional.softmax(outputs_unlab, dim=1).detach(), reduction='batchmean') 
+
+            loss = loss_lab + loss_unlab
+
+            ''' 
+            loss_unlab = torch.nn.functional.kl_div( 
+                         torch.nn.functional.log_softmax(outputs_unlab_aug/args.softmax_temp, dim=1), 
+                         torch.nn.functional.softmax(outputs_unlab/args.softmax_temp, dim=1).detach(), reduction='batchmean') 
+            '''
 
         loss.backward()
         optimizer.step()
@@ -266,19 +273,23 @@ def train(epoch, trainloader_lab, trainloader_unlab, scheduler, optimizer):
         
         train_loss += loss.item()
         train_loss_lab += loss_lab.item()
-        train_loss_unlab += loss_unlab.item()
-
+        if args.lab_only:
+            train_loss_unlab = 0.0
+        else:
+            train_loss_unlab += loss_unlab.item()
+        
         #progress_bar(i_iter, args.num_steps, 'Loss: %.6f | Loss_lab: %.6f'
             #% (loss.item(), loss_lab.item()))
-        progress_bar(i_iter, args.num_steps, 'Loss: %.6f | Loss_lab: %.6f | Loss_unlab: %.6f'
-            % (train_loss/1000.0, train_loss_lab/1000.0, train_loss_unlab/1000.0))
+        if args.verbose:
+            progress_bar(i_iter, args.num_steps, 'Loss: %.6f | Loss_lab: %.6f | Loss_unlab: %.6f'
+                % (train_loss/1000.0, train_loss_lab/1000.0, train_loss_unlab/1000.0))
 
         if i_iter%1000==0:
             train_loss /= 1000
             train_loss_lab /= 1000
             train_loss_unlab /= 1000
             
-            test(epoch, i_iter, train_loss, train_loss_lab, train_loss_unlab)
+            test(cycle, i_iter, train_loss, train_loss_lab, train_loss_unlab)
             val()        
             
             train_loss = 0
@@ -311,23 +322,25 @@ def val():
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
 
-            progress_bar(batch_idx, len(trainloader_val), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+            if args.verbose:
+                progress_bar(batch_idx, len(trainloader_val), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                    % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
      
 
-def test(epoch, i_iter, loss, loss_lab, loss_unlab):
+def test(cycle, i_iter, loss, loss_lab, loss_unlab):
     global best_acc
     net.eval()
     test_loss = 0
     correct = 0
     total = 0
     U_all = []
-    fp = open('results_semi_64_320_100k_w_flip_and_crop_20k_warm_up_sep_masks_wo_GCN_last_norm_again.txt','a') 
+    #filename = 'results_semi_' + str(args.batch_size_lab) + '_' + str(args.batch_size_unlab) + '_labels_' +  str(args.num_labeled)  + '_steps_' + str(args.num_steps)+ '_warm_' + str(args.warm_up_steps) + '_softmax_temp_' + str(args.softmax_temp) + '_conf_mask_' + str(args.confidence_mask) + '.txt'
+    filename = os.path.join(CHECKPOINT_DIR, 'results.txt')
+    #fp = open('results_semi_64_320_100k_w_flip_and_crop_20k_warm_up_sep_masks_wo_GCN_last_norm_again.txt','a') 
+    fp = open(filename, 'a')
     with torch.no_grad():
         for batch_idx, (inputs, targets) in enumerate(testloader):
             inputs, targets = inputs.to(device), targets.to(device)
-            #if args.GCN:
-                #inputs = global_contrast_normalize( inputs )
             outputs = net(inputs)
 
             probs = F.softmax(outputs, dim=1)
@@ -342,35 +355,28 @@ def test(epoch, i_iter, loss, loss_lab, loss_unlab):
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
 
-            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+            if args.verbose:
+                progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                    % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
      
     fp.write(str(i_iter) + ' ' + str(100.*correct/total) + ' loss: ' +  str(loss) + ' loss lab: ' + str(loss_lab) + ' loss unlab: ' + str(loss_unlab) + '\n')    
-    #fp.write(str(i_iter))    
-    
-    #U_all = torch.cat(U_all, dim=0)
-    #U_sorted, _ = U_all.sort()
-    #print ('average entropy: ', U_all.float().mean())
     
     # Save checkpoint.
     acc = 100.*correct/total
     if acc > best_acc:
         print('Saving..')
-        '''
+        
         state = {
             'net': net.state_dict(),
             'acc': acc,
-            'epoch': epoch,
+            'cycle': cycle,
         }
-        if not os.path.isdir('checkpoint'):
-            os.mkdir('checkpoint')
-        torch.save(state, './checkpoint/ckpt.t7')
-        '''
+        #if not os.path.isdir('checkpoint'):
+        #    os.mkdir('checkpoint')
+        #torch.save(state, './checkpoint/ckpt.t7')
+        torch.save(state, os.path.join(CHECKPOINT_DIR, 'best_ckpt.t7'))
+        
         best_acc = acc
     
-
-for epoch in range(start_epoch, start_epoch+100):
-    train(epoch, trainloader_lab, trainloader_unlab, scheduler, optimizer)
-    if epoch%5==0:
-        test(epoch)
-
+for cycle in range(args.num_cycles):
+    train(cycle, trainloader_lab, trainloader_unlab, scheduler, optimizer)
